d9a7651d945eac12b09489c0efde9f1923b46de8
GiancarloCordova <gcordova-2024016@kinal.edu.gt>
2025-08-20 10:57:13 -0600

    Se subio el proyecto sin nada trabajado de momento

---
+++ src/main/java/org/nexus/ap1clinica_dental/Ap1clinicaDentalApplication.java
@@ -0,0 +1,13 @@
+package org.nexus.ap1clinica_dental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ap1clinicaDentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ap1clinicaDentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/nexus/ap1clinica_dental/Ap1clinicaDentalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nexus.ap1clinica_dental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ap1clinicaDentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
