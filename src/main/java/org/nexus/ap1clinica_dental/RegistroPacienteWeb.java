b4b05621b82895201305f330292c85690fb147c7
GiancarloCordova <gcordova-2024016@kinal.edu.gt>
2025-08-26 07:07:54 -0600

    Se hicieron unas modificaciones

---
+++ src/main/java/org/nexus/ap1clinica_dental/RegistroPacienteWeb.java
@@ -0,0 +1,11 @@
+package org.nexus.ap1clinica_dental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroPacienteWeb {
+    public static void main(String[] args) {
+        SpringApplication.run(RegistroPacienteWeb.class, args);
+    }
+}
