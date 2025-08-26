3068400342e5a59f1b7024a4d5110e26b53fc02d
GiancarloCordova <gcordova-2024016@kinal.edu.gt>
2025-08-26 11:56:54 -0600

    Se guardaron los cambios

--- src/main/java/org/nexus/ap1clinica_dental/controller/IndexController.java
+++ src/main/java/org/nexus/ap1clinica_dental/controller/IndexController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.jsf.FacesContextUtils;
 //import jakarta.faces.view.ViewScoped;
 
 import java.util.List;
@@ -35,5 +36,40 @@ public class IndexController {
         this.pacientes = this.pacienteService.visualizarPacientes();
         this.pacientes.forEach(paciente -> logger.info(paciente.toString()));
     }
+
+    public void agregarPaciente(){
+        this.pacienteSeleccionado = new Paciente();
+    }
+
+    public void guardarPaciente(){
+        logger.info("Cliente a guardar: " + this.pacienteSeleccionado);
+        //Agregar
+        if (this.pacienteSeleccionado.getIdPaciente() ==null){
+            this.pacienteService.guardarPaciente(this.pacienteSeleccionado);
+            this.pacientes.add(this.pacienteSeleccionado);
+        }
+        //Modificar
+        else {
+            this.pacienteService.guardarPaciente(this.pacienteSeleccionado);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Paciente actualizado"));
+        }
+        PrimeFaces.current().executeScript("PF('ventanaModalPaciente').hide");
+        PrimeFaces.current().ajax().update("formulario-pacientes:mensaje-emergente","formulario-pacientes:tabla-pacientes");
+        this.pacienteSeleccionado = null;
+    }
+
+    public void eliminarPaciente(){
+        logger.info("Cliente a eliminar: " + this.pacienteSeleccionado);
+        this.pacienteService.eliminarPaciente(this.pacienteSeleccionado);
+        this.pacientes.remove(this.pacienteSeleccionado);
+        this.pacienteSeleccionado = null;
+        FacesContext.getCurrentInstance().addMessage(
+                null, new FacesMessage("Cliente eliminado"));
+        PrimeFaces.current().ajax().update("formulario-pacientes:mensaje-emergente",
+                "formulario-pacientes:tabla-pacientes");
+
+    }
+
+
 
 }
